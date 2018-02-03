05a29e1c558274203d99efcdb0fa5558e2769a15
FND <fnd@localhost.localdomain>
2018-02-03 11:05:13 +0100

    upgraded dependencies

--- faucet.config.js
+++ faucet.config.js
@@ -1,23 +1,13 @@
 "use strict";
 
-let sass = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "./lib/styles/index.scss",
-		target: "./assets/bundle.css"
-	}]
-};
-
-let js = {
-	manifest: false,
-	bundles: [{
-		entryPoint: "./lib/scripts/index.js",
-		target: "./assets/bundle.js"
-	}]
-};
-
 module.exports = {
 	watchDirs: ["./lib", "./components"],
-	sass,
-	js
+	sass: [{
+		source: "./lib/styles/index.scss",
+		target: "./assets/bundle.css"
+	}],
+	js: [{
+		source: "./lib/scripts/index.js",
+		target: "./assets/bundle.js"
+	}]
 };
